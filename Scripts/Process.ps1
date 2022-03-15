189c89d33bb6dbe74d655732a7a0f0bb8a6d53ba
laqieer <laqieer@126.com>
2022-03-15 23:19:10 +0800

    Script helps to process new ROMs

---
+++ Scripts/Process.ps1
@@ -0,0 +1,21 @@
+[CmdLetbinding()]
+param (
+[decimal]$Number,
+[string]$ROM
+)
+
+Import-Module .\crc32.psm1
+
+$Basename = [System.IO.Path]::GetFileNameWithoutExtension($ROM)
+$Directory = [System.IO.Path]::GetDirectoryName($ROM)
+$ImgPath = "..\OfflineList\imgs\laqieer - Fire Emblem - Game Boy Advance\1-500\"
+
+Get-Item $ROM
+get-crc32 $ROM
+Compress-Archive -Force -LiteralPath $ROM -DestinationPath ..\ROMs\$Basename.zip
+Remove-Item $ROM
+
+get-crc32 $Directory\$Basename-0.png
+get-crc32 $Directory\$Basename-1.png
+Move-Item $Directory\$Basename-0.png $ImgPath"$Number"a.png
+Move-Item $Directory\$Basename-1.png $ImgPath"$Number"b.png
